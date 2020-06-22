5d7169a05c3919d036b989cd4ea8a9b7a675337e
ADIthaker <adityathaker28@gmail.com>
2020-06-22 13:35:51 +0530

    addederror404inappjs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const shopRoutes = require('./routes/shop');
 
-app.get('/', (req, res, next) => {
-    res.send("Hello");
-})
+app.set('view-engine', 'ejs');
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).render('404.ejs');
+});
 
 app.listen(5000, () => {
     console.log("Listening on port 5000")
